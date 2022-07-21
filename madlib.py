007a2dde9613aeb04ffe75736cc016aa7fdd2f99
Sam Brindle <brindlejsam@gmail.com>
2022-07-20 19:42:12 -0700

    converted one big file into individual functions. able to get full functionality but running into ModuleNotFoundError and it is past TA time so i will have to submit.

--- madlib.py
+++ madlib.py
@@ -1,10 +1,85 @@
-def welcome():
-    print("***********************************************")
-    print("**     Welcome to Mad Matt's Madlib Maker    **")
-    print("**      You will be provided with some       **")
-    print("**   text that has {Adjective's}, {Noun's}   **")
-    print("**    {Verb's}, or {Adverb's}. As a user     **")
-    print("**  you should submit words that fit these   **")
-    print("** categories to build your custom sentence. **")
-    print("***********************************************")
+import re
 
+def welcome():
+
+    print("""
+    ***********************************************
+    **     Welcome to Mad Matt's Madlib Maker    **
+    **      You will be provided with some       **
+    **   text that has {Adjective's}, {Noun's}   **
+    **    {Verb's}, or {Adverb's}. As a user     **
+    **  you should submit words that fit these   **
+    ** categories to build your custom sentence. **
+    ***********************************************
+    """)
+
+
+regex = r"(?<={).*?(?=})"
+# user_filler = []
+# unparsed_message = ""
+# def read_template(file_path):
+
+# with open("assets/example1.txt") as file:
+#     unparsed_message = file.read().strip()
+#     print(unparsed_message)
+
+# parts_of_speech = re.findall(regex, unparsed_message)
+# parsed_message = re.sub(regex,"",unparsed_message)
+# print(parsed_message)
+# print(parts_of_speech)
+
+# filled_message = parsed_message.format("dark", "stormy", "night")
+# print(filled_message)
+
+# for part in parts_of_speech:
+#     user_filler.append(input(f"{part} -> "))
+#
+# my_tuple = tuple(user_filler)
+#
+# print(my_tuple)
+#
+# def filled_message(list):
+#     filled_message = parsed_message.format(*list)
+#     print(filled_message)
+#
+# filled_message(my_tuple)
+
+
+def read_template(file_path):
+    with open(f"{file_path}") as file:
+        unparsed_message = file.read().strip()
+        return(unparsed_message)
+
+
+def parse_template(string):
+
+    def pieces(text, prompts):
+        stripped = text
+        parts = prompts
+        return stripped,parts
+
+    parts_of_speech = re.findall(regex, string)
+    parsed_message = re.sub(regex, "", string)
+
+    return pieces(parsed_message, tuple(parts_of_speech))
+
+
+def merge(string, prompts):
+    return string.format(*prompts)
+
+
+if __name__ == '__main__':
+    welcome()
+
+    unparsed_message = read_template("assets/example1.txt")
+    parsed_message, parts_of_speech = parse_template(unparsed_message)
+
+    user_filler = []
+    for part in parts_of_speech:
+        user_filler.append(input(f"{part} -> "))
+
+    filled_message = merge(parsed_message, tuple(user_filler))
+    print(filled_message)
+
+    with open("assets/finished_madlibs.txt", 'w') as file:
+        file.writelines(filled_message)
